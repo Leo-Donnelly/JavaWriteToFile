375e09da27e6edd82f4e02caaf8621e7df171449
Leo-Donnelly <leo.donnelly@2itesting.com>
2024-11-18 11:34:17 +0000

    Updates

--- src/writing/FileWrite.java
+++ src/writing/FileWrite.java
@@ -1,4 +1,28 @@
 package writing;
 
+import java.io.File;
+import java.io.IOException;
+
 public class FileWrite {
+
+    public static void main(String[] args) {
+
+        createFile();
+
+    }
+
+    public static void createFile (){
+        try {
+            File myFile = new File("C:\\Users\\LeoDonnelly\\IdeaProjects\\JavaWriteToFile\\src\\writing\\logins.txt");
+            if (myFile.createNewFile()) {
+                System.out.println("File created: " + myFile.getName());
+                System.out.println("Absolute path: " + myFile.getAbsolutePath());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
